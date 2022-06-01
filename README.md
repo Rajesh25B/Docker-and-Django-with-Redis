d5091099c25b08743e73530a264185c7fb156287
rajesh25b <basvoju.rajesh@outlook.com>
2022-06-01 13:15:22 +0530

    general setup of django env vars

---
+++ README.md
@@ -0,0 +1,16 @@
+# Docker with Django and Redis for storing Django Session cache data
+
+### stack used
+ - Python
+ - Django
+ - Docker / Docker-Compose
+ - Redis
+
+## Getting started
+
+To start project, run:
+
+```
+docker-compose up
+
+The project will then be available at http://127.0.0.1:8000
--- app/app/settings.py
+++ app/app/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+import environ
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3o3-!qovaa09#7m_wm(@j0=yakjm4csk%d3@ppqrv=%y3c8arj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
 
